1d6fadc0fc7fbc33e0bf73153a38ec9f018fa96c
asifalam515 <asibulalam515@gmail.com>
2022-10-17 01:34:13 +0600

    category card added

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const loadAllNewsCategory=()=>{
 
 // display all categories
 const displayCategoriesName=(categories)=>{
-    console.log(categories.news_category)
+    // console.log(categories.news_category)
     const data=categories.news_category
     const categoriesDiv=document.getElementById('categories')
 
@@ -21,8 +21,9 @@ const displayCategoriesName=(categories)=>{
 
             // const id=`${category.category_id} `
     categoryDiv.innerHTML=`
-    <btn   onClick=single("${category.category_id}") class="btn btn-primary" >${category.category_name} </btn>
+    <btn   onclick="singleCategory(${category.category_id})" class="btn btn-primary" >${category.category_name} </btn>
     `
+   
     
     
 // categoryDiv.innerText=`${category.category_name}`
@@ -36,11 +37,38 @@ const displayCategoriesName=(categories)=>{
 }
 
 // all news in a single category
-
-const single=(id)=>{
-    console.log(id);
+const singleCategory = (single) =>{
+    // console.log('get details of id', single);
+    const url = `https://openapi.programming-hero.com/api/news/category/0${single}`;
+    console.log(url);
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySingleDetails(data.data));
 }
+const displaySingleDetails = single =>{
+    console.log(single)
 
+    const detailContainer = document.getElementById('single-category');
+    detailContainer.innerHTML = ``;
+    const singleDiv = document.createElement('div');
+    singleDiv.classList.add('card');
+    singleDiv.innerHTML = `
+<div class="d-flex">
+<div class="w-25">
+<img  src="${single[0].image_url}" class="img-fluid" alt="...">
+</div>
+  <div class="card-body w-75">
+      <h5 class="card-title">${single[0].details}</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+
+</div>
+    `;
+    detailContainer.appendChild(singleDiv);
+}
 
 
 
