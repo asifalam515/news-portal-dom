0f4b612bc536b5004be3e6845ffb4810557b9dd6
asifalam515 <asibulalam515@gmail.com>
2022-10-17 08:40:03 +0600

    total view

--- js/app.js
+++ js/app.js
@@ -50,8 +50,14 @@ const displaySingleDetails = single =>{
     console.log(single)
     const detailContainer = document.getElementById('single-category');
     detailContainer.innerHTML = ``;
-   
+   if(single.length==0){
+    detailContainer.innerHTML=`
+    <h1 class="text-danger">No News Found</h1>
+    `
+    // console.log('not found');
+   }
     for(const singleNews of single){
+        
        console.log(singleNews.details.slice(0,200).concat('...'));
         console.log(singleNews.author.name)
         
@@ -59,14 +65,16 @@ const displaySingleDetails = single =>{
     const singleDiv = document.createElement('div');
     singleDiv.classList.add('card');
     singleDiv.innerHTML = `
-<div class="d-flex w-75">
+<div class="d-flex w-75 container justify-content-center">
 <div class="w-25">
-<img  src=" ${singleNews.image_url}?{singleNews.image_url}:"No images found" class="img-fluid">
+<img  class="img-fluid"  src=" ${singleNews.image_url ?singleNews.image_url :"No images found"}" >
 </div>
-  <div class="card-body w-75">
+  <div class="card-body w-75 m-3 p-2">
   <h5>${singleNews.title? singleNews.title :'No Tittle Found'}</h5>
-      <small class="card-title"> ${singleNews.details}?{singleNews.details}:'No Details Found'</small>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <small class="card-title"> ${singleNews.details ?singleNews.details.slice(0,200).concat('...') :'No Details Found'}</small>
+      <p class="card-text text-danger">${singleNews.author.name ?singleNews.author.name:"No Name Found"}</p>
+      <img  class="img-fluid rounded-5 w-25 h-50"  src=${singleNews.author.img ?singleNews.author.img:"No images found"} >
+      <p class="card-text text-info">Total View:${singleNews.total_view?singleNews.total_view:"No Name Found"}</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 
@@ -78,25 +86,7 @@ const displaySingleDetails = single =>{
 
     }
 
-//     detailContainer.innerHTML = ``;
-//     const singleDiv = document.createElement('div');
-//     singleDiv.classList.add('card');
-//     singleDiv.innerHTML = `
-// <div class="d-flex w-75">
-// <div class="w-25">
-// <img  src=" ${single[0].image_url}?{single[0].image_url}:"No images found" class="img-fluid">
-// </div>
-//   <div class="card-body w-75">
-//   <h5>${single[0].title? single[0].title :'No Tittle Found'}</h5>
-//       <small class="card-title"> ${single[0].details}?{single[0].details}:'No Details Found'</small>
-//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//       <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
 
-
-// </div>
-//     `;
-//     detailContainer.appendChild(singleDiv);
 }
 
 
